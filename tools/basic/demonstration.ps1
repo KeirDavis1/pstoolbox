435c9eb6a8fb44e37191be9998d0413bf2b9d29c
Keir Davis <keirjdavis@gmail.com>
2018-10-24 11:31:08 +0100

    Moved demonstration into basic folder

--- tools/basic/demonstration.ps1
+++ tools/basic/demonstration.ps1
@@ -1,12 +1,12 @@
 # Demo function to show how the tool can be used
 # Asks user how their eggs like to be cooked
 
-Function DemonstrationFunction {
+Function DemonstrationTool {
 	$response = Function_CreateInputBox -title "This is a demo" -description "How do you like your eggs?" -default "fried"
 	Write-Host "User likes their eggs $response"
 }
 
 $toolModule = @{
 	name = "Demonstration"
-	function = "DemonstrationFunction"
+	function = "DemonstrationTool"
 }
